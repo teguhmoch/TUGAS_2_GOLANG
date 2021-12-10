df51784019b42677e568dc81e27e5781cad3979e
teguhmochammad721@gmail.com <teguhmochammad721@gmail.com>
2021-12-10 17:39:48 +0700

    TUGAS 2 GOLANG
    membuat variable angka dengan kondisi Bilangan ganjil / genap

---
+++ belajargolang/tugas2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  var angka = 7;
+  if angka%2==0{
+    fmt.Println(angka,"adalah bilangan Genap")
+  }else{
+    fmt.Println(angka,"adalah bilangan Ganjil")
+  }
+}
